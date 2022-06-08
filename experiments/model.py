fdc9e688a840df70a96f79be6d557333ea9e2267
Ne0 <chenxi1228@foxmail.com>
2022-06-08 22:23:19 +0800

    Update model.py
    modify

--- experiments/model.py
+++ experiments/model.py
@@ -89,7 +89,7 @@ class Discriminator(nn.Module):
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
-    parser.add_argument("--n_epochs", type=int, default=50000, help="number of epochs of training")
+    parser.add_argument("--n_epochs", type=int, default=2000, help="number of epochs of training")
     parser.add_argument("--batch_size", type=int, default=64, help="size of the batches")
     parser.add_argument("--lr", type=float, default=0.0002, help="adam: learning rate")
     parser.add_argument("--b1", type=float, default=0.5, help="adam: decay of first order momentum of gradient")
